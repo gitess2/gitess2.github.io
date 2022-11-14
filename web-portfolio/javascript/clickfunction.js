7d8cd07fa3682a8a0e2bcb6ed98a8bbfd7bea041
Tess <hwalker2@unca.edu>
2022-11-13 23:50:35 -0500

    svg more likeahhhhhhhhh

--- web-portfolio/javascript/clickfunction.js
+++ web-portfolio/javascript/clickfunction.js
@@ -1,4 +1,9 @@
 document.querySelector('#rainbow').addEventListener('click', () => {
-    document.getElementById("title").innerHTML = "Add life to your website.",
+    document.getElementById("title").innerHTML = "Add life to your website.";
+    document.getElementById("arrow").innerHTML="<img src='images/arrow-down-long-solid.svg'>";
     document.querySelector('#title').classList.add('textOutline');
+    document.querySelector('#title').classList.add('fadeIn');
+    document.querySelector('#arrow').classList.add('fadeIn');
   });
+  
+ 
