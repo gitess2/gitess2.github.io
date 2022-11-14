791262f4d5cd4f2819b6f1c8a3c7dd92aa7e0e1a
Tess <hwalker2@unca.edu>
2022-11-13 21:41:21 -0500

    textie-poop

---
+++ web-portfolio/javascript/clickfunction.js
@@ -0,0 +1,4 @@
+document.querySelector('#rainbow').addEventListener('click', () => {
+    document.getElementById("title").innerHTML = "Add life to your website.",
+    document.querySelector('#title').classList.add('textOutline');
+  });
