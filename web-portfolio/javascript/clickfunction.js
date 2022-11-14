17e6473c42a1b1d68d01fe6bf3712e2722a52b57
Tess <hwalker2@unca.edu>
2022-11-14 04:05:42 -0500

    sure

--- web-portfolio/javascript/clickfunction.js
+++ web-portfolio/javascript/clickfunction.js
@@ -1,9 +1,18 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+    function hideScroll() {
+        document.body.style.overflow = 'hidden';
+    }
+
+hideScroll()
+
 document.querySelector('#rainbow').addEventListener('click', () => {
     document.getElementById("title").innerHTML = "Add life to your website.";
     document.getElementById("arrow").innerHTML="<img src='images/arrow-down-long-solid.svg'>";
     document.querySelector('#title').classList.add('textOutline');
     document.querySelector('#title').classList.add('fadeIn');
     document.querySelector('#arrow').classList.add('fadeIn');
-  });
-  
- 
+    document.body.style.overflow = 'visible';
+}); 
--- web-portfolio/javascript/index.js
+++ web-portfolio/javascript/index.js
@@ -16,7 +16,7 @@ const handleOnClick = index => {
   anime({
     targets: ".tile",
     opacity: toggled ? 0 : 1,
-    delay: anime.stagger(50, {
+    delay: anime.stagger(55, {
       grid: [columns, rows],
       from: index
     })
@@ -41,20 +41,20 @@ const createTiles = quantity => {
   });
 }
 
-const createGrid = () => {
+const createGrid = (rowsvar) => {
   wrapper.innerHTML = "";
   
-  const size = document.body.clientWidth > 800 ? 100 : 50;
+  const size = 100
   
   columns = Math.floor(document.body.clientWidth / size);
   rows = Math.floor(document.body.clientHeight / size);
   
   wrapper.style.setProperty("--columns", columns);
-  wrapper.style.setProperty("--rows", rows);
+  wrapper.style.setProperty("--rows", rowsvar);
   
   createTiles(columns * rows);
 }
 
 createGrid();
 
-window.onresize = () => createGrid();
+window.onresize = () => createGrid(rows / 1.875); /* made rows var return it to original state */
