85380855dab11c8afc568cbbd8a8902d6f82efe5
Tess <hwalker2@unca.edu>
2022-11-19 18:42:49 -0500

    anus

--- web-portfolio/javascript/clickfunction.js
+++ web-portfolio/javascript/clickfunction.js
@@ -16,3 +16,13 @@ document.querySelector('#rainbow').addEventListener('click', () => {
     document.querySelector('#arrow').classList.add('fadeIn');
     document.body.style.overflow = 'visible';
 }); 
+
+scroll(); {
+element.scrollTo({
+    top: 100,
+    left: 100,
+    behavior: 'smooth'
+  });
+}
+
+scroll()
